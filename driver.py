8ad20e40a9fcd17083c6555f271e3799a195ec16
Sam Liebman <sammyliebman@gmail.com>
2018-06-29 12:03:11 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,6 +1,16 @@
 # define Driver class and instance methods here
 
+class Driver:
+    def greeting(self):
+        return "Hey, how are you?"
+
+    def ask_for_destination(self):
+        return "Where would you like to go today?"
+
 # Note: remember that our instance methods need to be defined with the an
 # argument in order to be called on an instance of the class or else we will
 # get a `TypeError` telling us that we gave one too many arguments to the
 # function. We'll explain why that is soon.
+
+
+# In the Driver class, define the instance method greeting that returns the string "Hey, how are you?". Then define the method ask_for_destination, which returns the string "Where would you like to go today?".
--- passenger.py
+++ passenger.py
@@ -1,6 +1,16 @@
 # define Passenger class and instance methods here
 
+class Passenger:
+    def reply_greeting(self):
+        return "I am doing well! Thanks for picking me up today!"
+
+    def in_a_hurry(self):
+        return "Punch it! They're on our tail!"
+
 # Note: remember that our instance methods need to be defined with the an
 # argument in order to be called on an instance of the class or else we will
 # get a `TypeError` telling us that we gave one too many arguments to the
 # function. We'll explain why that is soon.
+
+
+# In the Passenger class, define the instance method reply_greeting that returns the string "I am doing well! Thanks for picking me up today!". Then define the method in_a_hurry, which returns the string "Punch it! They're on our tail!".
